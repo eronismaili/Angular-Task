b0b40c621775d71b4e903659009eed75e9e93363
eroni <eronismailii1@gmail.com>
2024-11-17 21:20:21 +0100

    update

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,15 +1,12 @@
 import { Routes } from '@angular/router';
-import { AuthService } from './auth.service';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { ProductsComponent } from './products/products.component';
-import { RegisterComponent } from './register/register.component';
-
+import { AuthGuard } from './auth/auth.guard';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
-  { path: 'register', component: RegisterComponent },
-  { path: 'home', component: HomeComponent,},
-  { path: 'products', component: ProductsComponent, },
+  { path: 'home', component: HomeComponent, canActivate: [AuthGuard] },
+  { path: 'products', component: ProductsComponent, canActivate: [AuthGuard] },
 ];
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -4,13 +4,23 @@ import { Router } from '@angular/router';
 import { AuthService } from '../auth.service';
 import { CommonModule } from '@angular/common';
 import {TranslatePipe} from "@ngx-translate/core";
+import {animate, state, style, transition, trigger} from "@angular/animations";
 
 @Component({
   selector: 'app-login',
   standalone: true,
   imports: [CommonModule, ReactiveFormsModule, TranslatePipe],
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
+  animations: [
+    trigger('state', [
+      state('done', style({
+      })),
+      transition('* => done', [
+        animate('0.5s')
+      ])
+    ])
+  ]
 })
 export class LoginComponent implements OnInit {
   loginForm!: FormGroup;
@@ -38,10 +48,21 @@ export class LoginComponent implements OnInit {
         },
         (error) => {
           console.error('Login failed', error);
+          if (error.status === 401) {
+            window.alert('Invalid credentials! Please try again.');
+          }
+          else if (error.status === 400) {
+            window.alert('User not found! Please check your email.');
+          } else if (error.status === 404) {
+            window.alert('User not found! Please check your email.');
+          } else {
+            window.alert('An error occurred. Please try again later.');
+          }
         }
       );
     } else {
       console.error('Form is not valid');
+      window.alert('Please enter valid email and password.');
     }
   }
 }
